d3dd9214cead59647d79b10672bbc6f9dfe55be6
devansh-dvj <devansh.dvj@gmail.com>
2021-05-11 22:53:59 +0530

    Updated patchmatcher - fixed indices as uint8

--- patchmatcher_test.m
+++ patchmatcher_test.m
@@ -7,7 +7,6 @@ rng(1);
 addpath("yuv4mpeg2mov");
 addpath("BM3D");
 
-
 mov = yuv4mpeg2mov("data/akiyo_qcif.y4m");
 
 frame1 = mov(1).cdata;
@@ -17,29 +16,26 @@ for i=1:size(mov, 2)
     frames(:,:,i) = rgb2gray(mov(i).cdata) + cast(randn(size(frame1, 1), size(frame1, 2))*20, 'uint8');
 end
 
-firstgroup=frames(:,:,1:50);
+firstgroup = frames(:,:,1:50);
 
-patchArr=zeros([8 8 35 43 50], 'uint8');
+patchArr = zeros([8 8 35 43 50], 'uint8');
 
 for i=1:50
     for j=1:35
         for k=1:43
-            patchArr(:,:,j,k,i)=firstgroup((1+4*(j-1)):(4*(j+1)),(1+4*(k-1)):(4*(k+1)),i);
+            patchArr(:,:,j,k,i) = firstgroup((1+4*(j-1)):(4*(j+1)), (1+4*(k-1)):(4*(k+1)), i);
         end
     end
 end
 
-new=reshape(patchArr, [64 35 43 50]);
+new = reshape(patchArr, [64 35 43 50]);
 
-myindices=patchmatcher(new,10,11,25);
-% size(myindices)
+indices = patchmatcher(new, 10, 11, 25);
 
 figure;
-tiledlayout(3,3)
+tiledlayout(3, 3)
 
 for i=11:19
     nexttile;
-    imshow(patchArr(:,:,myindices(1,i), myindices(2,i), myindices(3,i)));
+    imshow(patchArr(:, :, indices(1,i), indices(2,i), indices(3,i)));
 end
-
-
