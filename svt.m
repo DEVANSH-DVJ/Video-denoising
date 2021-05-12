3e7cb8f90d63ad38967df887658bbf14bb41dd84
devansh-dvj <devansh.dvj@gmail.com>
2021-05-12 23:33:43 +0530

    Fixed bug in svt

--- svt.m
+++ svt.m
@@ -10,12 +10,15 @@ function [Q, iter]=svt(P, Omega, tau)
     sigma_bar = zeros(64, 1, 'double');
     for i=1:64
         Pi = P(i,:);
-        sigma_bar(i) = std(Pi(Omega(:,i)));
+        sigma_bar(i) = std(Pi(Omega(i,:)));
+%         mu_bar = mean(Pi(Omega(i,:)));
+%         Omega(i,:) = Omega(i,:) & (Pi <= mu_bar + 2 * sigma_bar(i)) & (Pi >= mu_bar - 2 * sigma_bar(i));
     end
     sigma_hat = mean(sigma_bar);
 
     mu = (8 + sqrt(n2)) * sqrt(p) * sigma_hat;
     n = min(64, n2);
+    lambda = tau*mu;
 
     Q = zeros(size(P), 'double');
 
@@ -26,9 +29,9 @@ function [Q, iter]=svt(P, Omega, tau)
         R = Q - tau * P_;
         [U, S, V] = svd(R);
 
-        Q_new = U(:,1:n) * diag(max(diag(S) - tau*mu, 0)) * V(:,1:n)';
+        Q_new = U(:,1:n) * diag(max(diag(S) - lambda, 0)) * V(:,1:n)';
 
-        if (norm(Q_new - Q, 'fro') <= 1e-5)
+        if (norm(Q_new - Q, 'fro') <= 1e-4)
             break;
         end
 
@@ -37,5 +40,3 @@ function [Q, iter]=svt(P, Omega, tau)
 
     iter = k;
 end
-
-
