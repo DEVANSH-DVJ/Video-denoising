e288cef3182a3e7c29b27fee0bfff5ecb5631852
devansh-dvj <devansh.dvj@gmail.com>
2021-05-14 13:51:04 +0530

    Ran updated experiments on 10,10,10

--- experiments/grayscale/PCA_test1.m
+++ experiments/grayscale/PCA_test1.m
@@ -14,7 +14,7 @@ mov = yuv4mpeg2mov('../../data/carphone_qcif.y4m');
 % Update these parameters
 sigma = 10;
 k = 10;
-s = 20;
+s = 10;
 
 dim1 = size(mov(1).cdata, 1);
 dim2 = size(mov(1).cdata, 2);
