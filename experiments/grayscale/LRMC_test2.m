0423e183774f19bed75194e69b41057c070c05e9
devansh-dvj <devansh.dvj@gmail.com>
2021-05-14 07:26:33 +0530

    Ran updated experiments on 50,5,50

--- experiments/grayscale/LRMC_test2.m
+++ experiments/grayscale/LRMC_test2.m
@@ -14,7 +14,7 @@ mov = yuv4mpeg2mov('../../data/carphone_qcif.y4m');
 % Update these parameters
 sigma = 50;
 k = 05;
-s = 30;
+s = 50;
 
 dim1 = size(mov(1).cdata, 1);
 dim2 = size(mov(1).cdata, 2);
