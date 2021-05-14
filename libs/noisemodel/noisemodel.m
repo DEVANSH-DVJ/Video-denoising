06518fc2b6538a16b66c77a7637331f4fbb06ff9
devansh-dvj <devansh.dvj@gmail.com>
2021-05-14 06:42:28 +0530

    Added comments to libs

--- libs/noisemodel/noisemodel.m
+++ libs/noisemodel/noisemodel.m
@@ -1,24 +1,29 @@
 
 function [noisy]=noisemodel(frames,sigma,k,s)
-    % assume frames is of size ([dim1 dim2 nframes]) (uint8)
-    % We have to add gaussian, possion, impulsive noise
+    % Input:
+    %   frames : original video, [dim1 dim2 nframes] (uint8)
+    %   sigma  : std. deviation of gaussian noise
+    %   k      : variance factor of the poission noise
+    %   s      : percentage of impulsive noise
+    % Output:
+    %   noisy : noisy video, [dim1 dim2 nframes] (uint8)
+    % Brief:
+    %   Add gaussian, poisson, impulsive noise to the video
 
-    % Output would be noise added frames of same dimension
-
-    dim1=size(frames, 1);
-    dim2=size(frames, 2);
-    nframes=size(frames, 3);
+    [dim1 dim2 nframes] = size(frames);
 
     noisy = cast(frames, 'double');
+    % Poisson Noise with zero mean, variance k*noisy
     np = (poissrnd(k*noisy) - k*noisy);
+    % Gaussian Noise with zero mean, variance sigma^2
     ng = sigma*randn(dim1, dim2, nframes);
     noisy = noisy + np + ng;
 
+    % Using uniform normal distribution for Impulsive noise
     X = rand(dim1, dim2, nframes);
     noisy(X < s/200) = 0;
     noisy(X > 1 - s/200) = 255;
 
+    % Cast into uint8 to remove pixel intensity not in [0, 255]
     noisy = cast(noisy, 'uint8');
-    noisy(noisy < 0) = 0;
-    noisy(noisy > 255)= 255;
 end
