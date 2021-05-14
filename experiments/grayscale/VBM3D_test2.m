1960bab41f728a05cf875e4f9f9d79bf242f796e
devansh-dvj <devansh.dvj@gmail.com>
2021-05-14 13:33:45 +0530

    Ran updated experiments on 10,10,50

--- experiments/grayscale/VBM3D_test2.m
+++ experiments/grayscale/VBM3D_test2.m
@@ -12,9 +12,9 @@ addpath('../../libs/noisemodel');
 mov = yuv4mpeg2mov('../../data/carphone_qcif.y4m');
 
 % Update these parameters
-sigma = 50;
-k = 05;
-s = 00;
+sigma = 10;
+k = 10;
+s = 50;
 
 dim1 = size(mov(1).cdata, 1);
 dim2 = size(mov(1).cdata, 2);
